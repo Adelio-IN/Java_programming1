61fb0c40d7b4cb1dec0330e03c272477a4001995
LEE IN <2024011862@office.cju.ac.kr>
2025-03-27 16:00:56 +0900

    personal_information 파일 생성 및 코드 작성

---
+++ personal_information.java
@@ -0,0 +1,10 @@
+public class personal_information
+{
+    public static void main(String[] args)
+    {
+        int age = 21;
+        String name = "LEE IN";
+        System.out.println(name + "님은" + age + "살입니다.");
+    }
+}
+
